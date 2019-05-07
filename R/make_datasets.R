72b41cb13d093b5b7c7ffb7f41cfeb28d81f7ba0
Francis Banville <francis.banville@umontreal.ca>
2019-05-07 14:09:41 -0400

    import data

---
+++ R/make_datasets.R
@@ -0,0 +1,16 @@
+
+
+
+nla.2007.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/lake_profiles/nla2007_profile_20091008.csv", header = TRUE)
+nla.2012.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/lake_profiles/nla2012_wide_profile_08232016.csv", header = TRUE)
+
+str(nla.2007.profile)
+str(nla.2012.profile)
+
+View(head(nla.2007.profile))
+View(head(nla.2012.profile))
+
+dim(nla.2007.profile)
+dim(nla.2012.profile)
+
+
