f68bb0d050bb1b5ba6e2854fea8067dd59ce9ba3
Francis Banville <francis.banville@umontreal.ca>
2019-05-07 14:25:49 -0400

    change of data sets folder

--- R/make_datasets.R
+++ R/make_datasets.R
@@ -1,8 +1,8 @@
 
 
 
-nla.2007.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/lake_profiles/nla2007_profile_20091008.csv", header = TRUE)
-nla.2012.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/lake_profiles/nla2012_wide_profile_08232016.csv", header = TRUE)
+nla.2007.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/raw/lakes_profile/nla2007_profile_20091008.csv", header = TRUE)
+nla.2012.profile = read.csv("C:/Users/Francis Banville/Documents/Biologie_quantitative_et_computationnelle/Travaux_dirigés/Travail_dirige_II/US_LakeProfiles/data/raw/lakes_profile/nla2012_wide_profile_08232016.csv", header = TRUE)
 
 str(nla.2007.profile)
 str(nla.2012.profile)
